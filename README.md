d23f4445c82e30514ad81f32b1ef53a69129b1d1
Matheus Farina <farinamatheus07@gmail.com>
2022-06-07 23:01:19 -0300

    MORE WIDGETS!

--- README.md
+++ README.md
@@ -7,10 +7,23 @@ This is my first Flutter project designed to get a grasp of the basics in what F
 So far on the first update. Not much has been done. Just basic understanding of how Flutter exhibits text on screen and how Columns and Text Direction works.
 
 Tests done on Pixel 4 (Emulated) and on browser, Chrome.
-### notes
+
+## Second Update
+Putting more widgets to use, AppBar, FloatingActionButton and more.
+
+Learning how to work on Stateful and Stateless Widgets
+
+
+<hr>
+
+### Personal Notes
 Flutter <b>do</b> work as a charm so far
 
-<br>
+
+I admit i should have been more dedicated to this project, months since the last update.
+Still. I'm learning a lot. v0.2
+
+<hr>
 
 ####Contact Info
 Twitter @MatheusFarina
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Column(
-    children:  <Widget>[
-      const Text(
-        'Deliver features faster',
-        textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    home: Scaffold(
+      body:ListaTransferencias(),
+      appBar: AppBar(title: Text('Transferências'),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
       ),
-      const Text(
-        'Craft beautiful UIs',
-        textDirection: TextDirection.ltr,
-      ),
-      const Text(
-        'Craft beautiful UIs',
-        textDirection: TextDirection.ltr,
-              ),
-      const Text(
-        'data',
-        textDirection: TextDirection.rtl,
-      )
-      ,Column(
-        children: const[
-             Text('Test', textDirection: TextDirection.ltr,)
-        ],
-      ),
-    ],
+    )
   ));
+}
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.0,1000)),
+        ItemTransferencia(Transferencia(200.0,2000)),
+        ItemTransferencia(Transferencia(300.0,2034)),
+      ],
+    );
+
+  }
+  
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+final Transferencia _transferencia;
+
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
